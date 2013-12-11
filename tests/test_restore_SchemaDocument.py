471c04dd244af062506c0a3dc9f226b0573ff6fe
Bertrand Néron <bneron@pasteur.fr>
2013-12-11 14:48:37 +0100

    add unit test to test the Schemadocument restoration
    a registered SchemaDocument is restore as a new object instead of dict
    so this tests test this feature

---
+++ tests/test_restore_SchemaDocument.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+
+#==============================================
+#Created on Dec 11, 2013
+#
+#@author: Bertrand Néron
+#@contact: bneron@pasteur.fr
+#@organization: Institut Pasteur
+#==============================================
+
+
+import unittest
+
+from mongokit import *
+
+class RestoreSchemaDocumentTestCase(unittest.TestCase):
+    def setUp(self):
+        self.connection = Connection()
+        self.col = self.connection['test']['mongokit']
+
+    def tearDown(self):
+        self.connection.drop_database('test')
+
+    def test_restore_SD(self):
+        class Pet(SchemaDocument):
+            structure = {'name' : basestring }
+  
+        @self.connection.register    
+        class Canary(Pet):
+    
+            def say_hi(self):
+                print "I tawt I taw a puddy tat!"
+            
+        @self.connection.register            
+        class Cat(Pet):
+            structure = {
+                         'catch': Pet
+                         }
+    
+            def say_hi(self):
+                print "Sufferin' succotash"
+        
+        class Dog(Pet):
+            structure = {
+                         'catch': Pet
+                         }
+    
+            def say_hi(self):
+                print "ouaf ouaf"
+        
+        @self.connection.register
+        class GrandMa(Document):
+            __database__ = 'test'
+            __collection__ = 'granny'
+            structure = {
+                     '_type' : unicode,
+                     'bird' : Canary,
+                     'cat' : Cat,
+                     'dog' : Dog,
+                     'pets' : [Pet]
+                     }
+        tweety = Canary()
+        tweety['name'] = 'Tweety'
+
+        sylvester = Cat()
+        sylvester['name'] = 'Sylvester'
+        sylvester['catch'] = tweety
+
+        hector = Dog()
+        hector['name'] = 'Spike'
+        hector['catch'] = sylvester        
+        
+        granny= self.connection.GrandMa()
+        granny['bird'] = tweety
+        granny['cat'] = sylvester
+        granny['dog'] = hector
+        granny.save()
+        meme_reloaded = self.connection.GrandMa.fetch_one({})
+        self.assertEqual(type(meme_reloaded['bird']), type(tweety))
+        self.assertEqual(meme_reloaded['bird']['name'], 'Tweety')
+        self.assertEqual(type(meme_reloaded['cat']), type(sylvester))
+        self.assertEqual(meme_reloaded['cat']['name'], 'Sylvester')
+        self.assertEqual(type(meme_reloaded['dog']), type({}))
+        self.assertEqual(meme_reloaded['dog']['name'], 'Spike')
+        
+    def test_restore_SD_in_list(self):
+        class Pet(SchemaDocument):
+            structure = {'name' : basestring }
+  
+        @self.connection.register    
+        class Canary(Pet):
+    
+            def say_hi(self):
+                print "I tawt I taw a puddy tat!"
+            
+        @self.connection.register            
+        class Cat(Pet):
+            structure = {
+                         'catch': Pet
+                         }
+    
+            def say_hi(self):
+                print "Sufferin' succotash"
+        
+        class Dog(Pet):
+            structure = {
+                         'catch': Pet
+                         }
+    
+            def say_hi(self):
+                print "ouaf ouaf"
+        
+        @self.connection.register
+        class GrandMa(Document):
+            __database__ = 'test'
+            __collection__ = 'granny'
+            structure = {
+                     '_type' : unicode,
+                     'bird' : Canary,
+                     'cat' : Cat,
+                     'dog' : Dog,
+                     'pets' : [Pet]
+                     }
+        tweety = Canary()
+        tweety['name'] = 'Tweety'
+
+        sylvester = Cat()
+        sylvester['name'] = 'Sylvester'
+        sylvester['catch'] = tweety
+
+        hector = Dog()
+        hector['name'] = 'Spike'
+        hector['catch'] = sylvester        
+        
+        granny= self.connection.GrandMa()
+        granny['pets'] = [tweety, sylvester , hector]
+        granny.save()
+        meme_reloaded = self.connection.GrandMa.fetch_one({})
+        self.assertEqual(type(meme_reloaded['pets']), type([]))
+        self.assertEqual(type(meme_reloaded['pets'][0]), type(tweety))
+        self.assertEqual(type(meme_reloaded['pets'][1]), type(sylvester))
+        self.assertEqual(type(meme_reloaded['pets'][2]), type({}))        
+        
+    
+    def test_restore_recursive_SD(self):
+        class Pet(SchemaDocument):
+            structure = {'name' : basestring }
+  
+        @self.connection.register    
+        class Canary(Pet):
+    
+            def say_hi(self):
+                print "I tawt I taw a puddy tat!"
+            
+        @self.connection.register            
+        class Cat(Pet):
+            structure = {
+                         'catch': Pet
+                         }
+    
+            def say_hi(self):
+                print "Sufferin' succotash"
+        
+        class Dog(Pet):
+            structure = {
+                         'catch': Pet
+                         }
+    
+            def say_hi(self):
+                print "ouaf ouaf"
+        
+        @self.connection.register
+        class GrandMa(Document):
+            __database__ = 'test'
+            __collection__ = 'granny'
+            structure = {
+                     '_type' : unicode,
+                     'bird' : Canary,
+                     'cat' : Cat,
+                     'dog' : Dog,
+                     'pets' : [Pet]
+                     }
+        tweety = Canary()
+        tweety['name'] = 'Tweety'
+
+        sylvester = Cat()
+        sylvester['name'] = 'Sylvester'
+        sylvester['catch'] = tweety
+
+        hector = Dog()
+        hector['name'] = 'Spike'
+        hector['catch'] = sylvester        
+        
+        granny= self.connection.GrandMa()
+        granny['bird'] = tweety
+        granny['cat'] = sylvester
+        granny['dog'] = hector
+        granny['pets'] = [tweety, sylvester , hector]
+        granny.save()
+        meme_reloaded = self.connection.GrandMa.fetch_one({})
+        sylvester_2 = meme_reloaded['cat']
+        self.assertEqual(type(sylvester_2['catch']), type(tweety))
+        self.assertEqual(sylvester_2['catch']['name'], 'Tweety')
+        hector_2 = meme_reloaded['dog']
+        self.assertEqual(type(hector_2['catch']), type({}))
+        self.assertEqual(hector_2['catch']['name'], 'Sylvester')        
+        
+    
